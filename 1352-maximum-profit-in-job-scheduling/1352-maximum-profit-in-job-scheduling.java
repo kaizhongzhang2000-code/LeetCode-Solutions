3582a14b51a53cbc93e8b8d42a2a407ae760735b
kaizhongzhang2000-code <kaizhongzhang2000@gmail.com>
2026-05-25 13:11:47 -0700

    1352-MAXIMUM-PROFIT-IN-JOB-SCHEDULING Stats: Time: 2000 ms (5.03%), Space: 66.8 MB (80.28%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.java
@@ -1,53 +1,42 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int[][] info = new int[startTime.length][3];
-        for(int i = 0; i < info.length; i++){
-            info[i][0] = startTime[i];
-            info[i][1] = endTime[i];
-            info[i][2] = profit[i];
+        int[][] allInfo = new int[startTime.length][3];
+        for(int i = 0; i < startTime.length; i++){
+            allInfo[i] = new int[]{startTime[i], endTime[i], profit[i]};
         }
-        Arrays.sort(info, (a, b) -> {
-            int compare = Integer.compare(a[0], b[0]);
+        Arrays.sort(allInfo, (a, b) -> {
+            int compare = Integer.compare(a[1], b[1]);
             if(compare == 0){
-                compare = Integer.compare(a[1], b[1]);
+                compare = Integer.compare(b[0], a[0]);
+            }
+            if(compare == 0){
+                compare = Integer.compare(a[2], b[2]);
             }
             return compare;
         });
 
-        int[] tracker = new int[info.length];
-        Arrays.fill(tracker, -1);
-        return dp(info, 0, tracker);
-    }
-
-    public int dp(int[][] info, int index, int[] tracker){
-        if(index >= info.length){
-            return 0;
-        }
-        if(tracker[index] >= 0){
-           return tracker[index];
-        }
-        int skipProfit = dp(info, index + 1, tracker);
-        int nextIndex = binarySearch(info, index);
-        int adoptProfit = info[index][2] + dp(info, nextIndex, tracker);
-        int maxProfit = Math.max(skipProfit, adoptProfit);
-        tracker[index] = maxProfit;
-        return maxProfit;
-    }
-
-    public int binarySearch(int[][] info, int index){
-        int min = index + 1;
-        int max = info.length - 1;
-        int target = info[index][1];
-        int result = info.length;
-        while(max >= min){
-            int curr = (max + min) / 2;
-            if(info[curr][0] >= target){
-                result = curr;
-                max = curr - 1;
-            } else {
-                min = curr + 1;
+        int[] dp = new int[allInfo.length];
+        int max = 0;
+        for(int i = 0; i < allInfo.length; i++){
+            int lastIndex = i - 1;
+            while(lastIndex >= 0){
+                if(allInfo[lastIndex][1] <= allInfo[i][0]){
+                    break;
+                }
+                lastIndex--;
             }
+            int prevMax = 0;
+            if(lastIndex >= 0){
+                prevMax = dp[lastIndex];
+            }
+            int prev = 0;
+            if(i - 1 >= 0){
+                prev = dp[i - 1];
+            }
+            dp[i] = Math.max(prev, prevMax + allInfo[i][2]);
+            max = Math.max(max, dp[i]);
         }
-        return result;
+        return max;
+        
     }
 }
