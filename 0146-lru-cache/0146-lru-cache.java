7279a69e77e464c648d3bc5f899ce93157ab9f07
kaizhongzhang2000-code <kaizhongzhang2000@gmail.com>
2026-05-25 19:25:10 -0700

    0146-LRU-CACHE Stats: Time: 47 ms (53.47%), Space: 130.4 MB (90.46%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,34 +1,85 @@
 class LRUCache {
-    Set<Integer> recentSequence = new LinkedHashSet<>();
-    Map<Integer, Integer> timeMap = new HashMap<>();
     int capacity;
+    ListNode start = new ListNode();
+    ListNode end = new ListNode();
+    Map<Integer, ListNode> nodeMap = new HashMap<>();
     public LRUCache(int capacity) {
+        this.start.next = this.end;
+        this.end.prev = this.start;
         this.capacity = capacity;
     }
     
     public int get(int key) {
-        if(this.recentSequence.contains(key)){
-            this.recentSequence.remove(key);
-            this.recentSequence.add(key);
+        if(!nodeMap.containsKey(key)){
+            return -1;
         }
-        int value = this.timeMap.getOrDefault(key, -1);
-        return value;
+        ListNode currNode = nodeMap.get(key);
+        ListNode prevNode = currNode.prev;
+        ListNode nextNode = currNode.next;
+        ListNode newPrev = this.end.prev;
+         if(!newPrev.equals(currNode)){
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+            newPrev.next = currNode;
+            currNode.prev = newPrev;
+            currNode.next = this.end;
+            this.end.prev = currNode;
+         }
+        return currNode.val;
     }
     
     public void put(int key, int value) {
-        if(this.recentSequence.contains(key)){
-            this.recentSequence.remove(key);
-            this.recentSequence.add(key);
-            this.timeMap.put(key, value);
-        } else {
-            if(this.recentSequence.size() >= this.capacity){
-                int toRemove = this.recentSequence.iterator().next();
-                this.recentSequence.remove(toRemove);
-                this.timeMap.remove(toRemove);
+        if(nodeMap.containsKey(key)){
+            ListNode currNode = nodeMap.get(key);
+            ListNode prevNode = currNode.prev;
+            ListNode nextNode = currNode.next;
+            ListNode newPrev = this.end.prev;
+            if(!newPrev.equals(currNode)){
+                prevNode.next = nextNode;
+                nextNode.prev = prevNode;
+                newPrev.next = currNode;
+                currNode.prev = newPrev;
+                currNode.next = this.end;
+                this.end.prev = currNode;
             }
-            this.recentSequence.add(key);
-            this.timeMap.put(key, value);
+            currNode.val = value;
+        } else if (nodeMap.keySet().size() >= this.capacity){
+            ListNode remove = this.start.next;
+            ListNode newFirst = remove.next;
+            this.start.next = newFirst;
+            newFirst.prev = this.start;
+            nodeMap.remove(remove.key);
+            ListNode newNode = new ListNode(key, value);
+            ListNode prevNode = this.end.prev;
+            prevNode.next = newNode;
+            newNode.prev = prevNode;
+            newNode.next = this.end;
+            this.end.prev = newNode;
+            nodeMap.put(key, newNode);
+        } else {
+            ListNode newNode = new ListNode(key, value);
+            ListNode prevNode = this.end.prev;
+            prevNode.next = newNode;
+            newNode.prev = prevNode;
+            newNode.next = this.end;
+            this.end.prev = newNode;
+            nodeMap.put(key, newNode);
         }
+    }
+}
+
+class ListNode {
+    int key;
+    int val;
+    ListNode prev;
+    ListNode next;
+    public ListNode(){
+
+    }
+
+    public ListNode(int key, int val){
+        this.key = key;
+        this.val = val;
     }
 }
 
